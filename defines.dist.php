d6e60fea895810bec0c2c77c10a0a2658a649679
ghoulsblade <ghoulsblade@schattenkind.net>
2010-08-27 16:56:51 +0200

    config

---
+++ defines.dist.php
@@ -0,0 +1,7 @@
+<?php
+define("kMySQL_host"			,"localhost");
+define("kMySQL_user"			,"USERNAME");
+define("kMySQL_pass"			,"SUPERGEHEIMESPASSWORT");
+define("kMySQL_db"				,"DATENBANKNAME");
+define("kMySQL_SampleSoulID"	,"EINE_SEELEN_ID_FUER_DEN_DEMO_MODE__UNWICHTIG");
+?>
